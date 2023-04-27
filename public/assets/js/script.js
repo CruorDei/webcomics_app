a149e7dd2ca69f9636ac4d1acffad33555e393db
CruorDei <baptiste.denoue@gmail.com>
2023-04-27 23:00:52 +0200

    changement prod in cat and 'genre' in nav disabled (js too)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,31 +1,31 @@
 // JavaScript nav v1
-const button = document.getElementById("toggle-button");
-const element = document.getElementById("toggle-element");
+// const button = document.getElementById("toggle-button");
+// const element = document.getElementById("toggle-element");
 
-button.addEventListener("click", function() {
-  if (element.classList.contains("active")) {
-    // Si l'élément est actif, on le masque et on enlève la classe "active"
-    element.style.display = "none";
-    element.classList.remove("active");
-  } else {
-    // Sinon, on affiche l'élément et on ajoute la classe "active"
-    element.style.display = "flex";
-    element.classList.add("active");
-  }
-});
+// button.addEventListener("click", function() {
+//   if (element.classList.contains("active")) {
+//     // Si l'élément est actif, on le masque et on enlève la classe "active"
+//     element.style.display = "none";
+//     element.classList.remove("active");
+//   } else {
+//     // Sinon, on affiche l'élément et on ajoute la classe "active"
+//     element.style.display = "flex";
+//     element.classList.add("active");
+//   }
+// });
 
 //pagination accueil
 
 let currentPage = 1;
 
-document.getElementById('show-more').addEventListener('click', function () {
-currentPage++;
-if (currentPage > numPages) {
-document.getElementById('show-more').style.display = 'none';
-return;
-}
-let nextPage = document.getElementById('page' + currentPage);
-nextPage.style.display = 'grid';
+document.querySelector('#show-more').addEventListener('click', function show() {
+    currentPage++;
+    if (currentPage > numPages) {
+        document.querySelector('#show-more').style.display = 'none';
+        return;
+    }
+    let nextPage = document.querySelector('#page' + currentPage);
+    nextPage.style.display = 'grid';
 });
 
 //cat
